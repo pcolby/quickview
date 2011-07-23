14145a54fabcab0c9a220ec5e9468e34a02da132
Paul Colby <git@colby.id.au>
2011-07-23 01:22:36 +0000

    Updated the NSIS build script to use svnversion, and work with 64-bit Windows.

--- nsis/make.cmd
+++ nsis/make.cmd
@@ -1,14 +1,15 @@
 @echo off
 
-PATH=%PATH%;C:\Program Files\NSIS;C:\Program Files\SlikSvn\bin;C:\Program Files\UnxUtils\usr\local\wbin
+:: Make sure the NSIS, SlikSvn and UnxUtils binaries are in the path.
+PATH=%PATH%;C:\Program Files\NSIS;C:\Program Files (x86)\NSIS;C:\Program Files\SlikSvn\bin;C:\Program Files\UnxUtils\usr\local\wbin;C:\Program Files (x86)\UnxUtils\usr\local\wbin
 
-rem Set the REV_NO variable.
-svn info | grep "^Revision: " | cut -d" " -f2 > .revNo
-set /p REV_NO=<.revNo
-del .revNo
+:: Set the REVISION variable to the current subversion revision number.
+svnversion.exe .. | sed.exe -e "s/\([0-9]*:\)\?\([0-9]*\)\([MS]\)\?/\2/" > .revision
+set /p REVISION=<.revision
+del .revision
 
-rem Update the nsi script.
-sed -e "s/\(\([ 0-9]\+[.,]\)\{3\} *\)0/\1%REV_NO%/g" win32.nsi > win32.tmp.nsi
+:: Update the nsi script, and build the installer.
+sed -e "s/\(\([ 0-9]\+[.,]\)\{3\} *\)0/\1%REVISION%/g" win32.nsi > win32.tmp.nsi
 makensis win32.tmp.nsi
 rm win32.tmp.nsi
 
