470139c9ac1d688bf3661d8e6bcd3aa0ffc57b70
Paul Colby <git@colby.id.au>
2011-07-23 02:28:52 +0000

    A small code simplification (let the compiler sort it out).

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -143,7 +143,7 @@ void MainWindow::loadNextImage() {
     if (fileNamesIndex>=fileNames.count())
         fileNamesIndex=0; // Repeat all ;)
     pixmap.load(QString::fromAscii("%1/%2").arg(dirName).arg(fileNames.at(fileNamesIndex)));
-    setWindowIcon(QIcon(pixmap));
+    setWindowIcon(pixmap);
     updateWindowTitle();
 
     // Scale, and paint the new image.
